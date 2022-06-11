db87fd5ff06766894f71d5a67ca62c4733116bf8
Sam Komarov <sam@skomarov.com>
2022-06-11 12:21:34 +0300

    add router endpoints for new handlers
    Former-commit-id: 74bee29a2cf28574b7f7c593080ed501a9a9ec15

--- features/profiles/delivery/http/router/router.go
+++ features/profiles/delivery/http/router/router.go
@@ -6,10 +6,14 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func NewProfilesRouter(updateMe, updateAvatar, getMe http.HandlerFunc) func(r chi.Router) {
+func NewProfilesRouter(updateMe, updateAvatar, getMe, getById, getFollowsById, toggleFollow http.HandlerFunc) func(r chi.Router) {
 	return func(r chi.Router) {
 		r.Get("/me", getMe)
 		r.Put("/me", updateMe)
 		r.Put("/me/avatar", updateAvatar)
+
+		r.Get("/{id}", getById)
+		r.Get("/{id}/follow", getFollowsById)
+		r.Post("/{id}/toggle-follow", toggleFollow)
 	}
 }
--- features/profiles/profiles.go
+++ features/profiles/profiles.go
@@ -2,16 +2,10 @@ package profiles
 
 import (
 	"core/entities"
-	"core/image_decoder"
-	"core/static_file_creator"
 	"database/sql"
 	"log"
-	"profiles/delivery/http/handlers"
-	"profiles/delivery/http/router"
 	"profiles/domain/service"
-	"profiles/domain/validators"
 	"profiles/store"
-	"profiles/store/file_storage"
 	"profiles/store/sql_db"
 
 	"github.com/go-chi/chi/v5"
@@ -34,28 +28,30 @@ func NewRegisterCallback(db *sql.DB) func(auth.User) {
 }
 
 func NewProfilesRouterImpl(db *sql.DB) func(chi.Router) {
-	// db
-	sqlDB, err := sql_db.NewSqlDB(db)
-	if err != nil {
-		log.Fatalf("Error while opening sql db as a db for profiles: %v", err)
-	}
-	// file storage
-	avatarFileCreator := file_storage.NewAvatarFileCreator(static_file_creator.NewStaticFileCreatorImpl())
-	// store
-	storeDetailedProfileGetter := store.NewStoreDetailedProfileGetter(sqlDB.GetProfile)
-	storeProfileUpdater := store.NewStoreProfileUpdater(sqlDB.UpdateProfile, storeDetailedProfileGetter)
-	storeAvatarUpdater := store.NewStoreAvatarUpdater(avatarFileCreator, sqlDB.UpdateProfile)
-	// domain
-	profileUpdateValidator := validators.NewProfileUpdateValidator()
-	avatarValidator := validators.NewAvatarValidator(image_decoder.ImageDecoderImpl)
+	panic("unimplemented")
 
-	detailedProfileGetter := service.NewDetailedProfileGetter(storeDetailedProfileGetter)
-	profileUpdater := service.NewProfileUpdater(profileUpdateValidator, storeProfileUpdater)
-	avatarUpdater := service.NewAvatarUpdater(avatarValidator, storeAvatarUpdater)
-	// handlers
-	getMe := handlers.NewGetMeHandler(detailedProfileGetter)
-	updateMe := handlers.NewUpdateMeHandler(profileUpdater)
-	updateAvatar := handlers.NewUpdateAvatarHandler(avatarUpdater)
+	// // db
+	// sqlDB, err := sql_db.NewSqlDB(db)
+	// if err != nil {
+	// 	log.Fatalf("Error while opening sql db as a db for profiles: %v", err)
+	// }
+	// // file storage
+	// avatarFileCreator := file_storage.NewAvatarFileCreator(static_file_creator.NewStaticFileCreatorImpl())
+	// // store
+	// storeDetailedProfileGetter := store.NewStoreDetailedProfileGetter(sqlDB.GetProfile)
+	// storeProfileUpdater := store.NewStoreProfileUpdater(sqlDB.UpdateProfile, storeDetailedProfileGetter)
+	// storeAvatarUpdater := store.NewStoreAvatarUpdater(avatarFileCreator, sqlDB.UpdateProfile)
+	// // domain
+	// profileUpdateValidator := validators.NewProfileUpdateValidator()
+	// avatarValidator := validators.NewAvatarValidator(image_decoder.ImageDecoderImpl)
 
-	return router.NewProfilesRouter(updateMe, updateAvatar, getMe)
+	// detailedProfileGetter := service.NewDetailedProfileGetter(storeDetailedProfileGetter)
+	// profileUpdater := service.NewProfileUpdater(profileUpdateValidator, storeProfileUpdater)
+	// avatarUpdater := service.NewAvatarUpdater(avatarValidator, storeAvatarUpdater)
+	// // handlers
+	// getMe := handlers.NewGetMeHandler(detailedProfileGetter)
+	// updateMe := handlers.NewUpdateMeHandler(profileUpdater)
+	// updateAvatar := handlers.NewUpdateAvatarHandler(avatarUpdater)
+
+	// return router.NewProfilesRouter(updateMe, updateAvatar, getMe)
 }
