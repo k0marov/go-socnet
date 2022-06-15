8362b15fd13e9f2a8b47ed42bd45a879a94d6df8
Sam Komarov <sam@skomarov.com>
2022-06-15 09:28:37 +0300

    enforce Ref creation via the custom constructor
    Former-commit-id: e5a86e22a80dd5eba3735620f9422d8242cfd169

--- core/ref/ref.go
+++ core/ref/ref.go
@@ -3,17 +3,21 @@ package ref
 import "errors"
 
 // Ref is a pointer which can't be nil
-type Ref[T any] struct {
+type Ref[T any] interface {
+	Value() T
+}
+
+type ref[T any] struct {
 	pointer *T
 }
 
-func (r Ref[T]) Value() T {
+func (r ref[T]) Value() T {
 	return *r.pointer
 }
 
-func NewRef[T any](pointer *T) (Ref[T], error) {
+func NewRef[T any](pointer *T) (ref[T], error) {
 	if pointer == nil {
-		return Ref[T]{}, errors.New("references can't be created from a nil pointer")
+		return ref[T]{}, errors.New("references can't be created from a nil pointer")
 	}
-	return Ref[T]{pointer: pointer}, nil
+	return ref[T]{pointer: pointer}, nil
 }
