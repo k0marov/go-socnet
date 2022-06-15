6eaa2ad5ba5c00e354e3776b5df470bfb53cba0f
Sam Komarov <sam@skomarov.com>
2022-06-15 09:35:54 +0300

    change Ref back to struct
    Former-commit-id: fc265ad4b6c36a97e798dc0745512895b76f4990

--- core/ref/ref.go
+++ core/ref/ref.go
@@ -2,22 +2,18 @@ package ref
 
 import "errors"
 
-// Ref is a pointer which can't be nil
-type Ref[T any] interface {
-	Value() T
-}
-
-type ref[T any] struct {
+// ref is pointer that can't be nil
+type Ref[T any] struct {
 	pointer *T
 }
 
-func (r ref[T]) Value() T {
+func (r Ref[T]) Value() T {
 	return *r.pointer
 }
 
-func NewRef[T any](pointer *T) (ref[T], error) {
+func NewRef[T any](pointer *T) (Ref[T], error) {
 	if pointer == nil {
-		return ref[T]{}, errors.New("references can't be created from a nil pointer")
+		return Ref[T]{}, errors.New("references can't be created from a nil pointer")
 	}
-	return ref[T]{pointer: pointer}, nil
+	return Ref[T]{pointer: pointer}, nil
 }
