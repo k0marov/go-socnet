d48b969fd41f823232d1105bab12d3e9bb401824
Sam Komarov <sam@skomarov.com>
2022-06-15 15:22:11 +0300

    scaffold service layer
    Former-commit-id: aa1df9e4832845e641d7efed109ab58c5bf6cc11

--- features/posts/domain/service/service.go
+++ features/posts/domain/service/service.go
@@ -6,7 +6,33 @@ import (
 	"posts/domain/values"
 )
 
-type PostDeleter func(post values.PostId, fromUser core_values.UserId) error
-type PostLikeToggler func(values.PostId, core_values.UserId) error
-type PostCreator func(values.NewPostData) (entities.Post, error)
-type PostsGetter func(core_values.UserId) ([]entities.Post, error)
+type (
+	PostDeleter     func(post values.PostId, fromUser core_values.UserId) error
+	PostLikeToggler func(values.PostId, core_values.UserId) error
+	PostCreator     func(values.NewPostData) (entities.Post, error)
+	PostsGetter     func(authorId core_values.UserId) ([]entities.Post, error)
+)
+
+func NewPostDeleter() PostDeleter {
+	return func(post, fromUser core_values.UserId) error {
+		panic("unimplemented")
+	}
+}
+
+func NewPostLikeToggler() PostLikeToggler {
+	return func(postId values.PostId, userId core_values.UserId) error {
+		panic("unimplemented")
+	}
+}
+
+func NewPostCreator() PostCreator {
+	return func(newPost values.NewPostData) (entities.Post, error) {
+		panic("unimplemented")
+	}
+}
+
+func NewPostsGetter() PostsGetter {
+	return func(authorId core_values.UserId) ([]entities.Post, error) {
+		panic("unimplemented")
+	}
+}
--- features/posts/domain/service/service_test.go
+++ features/posts/domain/service/service_test.go
@@ -1,1 +1,19 @@
 package service_test
+
+import "testing"
+
+func TestPostsGetter(t *testing.T) {
+
+}
+
+func TestPostDeleter(t *testing.T) {
+
+}
+
+func TestPostCreator(t *testing.T) {
+
+}
+
+func TestPostLikeToggler(t *testing.T) {
+
+}
