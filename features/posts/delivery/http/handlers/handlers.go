4c9f0dfc0474610d923a564690a4ca02ccdd984d
Sam Komarov <sam@skomarov.com>
2022-06-15 06:08:21 +0300

    scaffold handlers
    Former-commit-id: 67689628772301cfd6559bbc244eec1c359f52cd

--- features/posts/delivery/http/handlers/handlers.go
+++ features/posts/delivery/http/handlers/handlers.go
@@ -1,1 +1,27 @@
 package handlers
+
+import "net/http"
+
+func NewDeleteHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func NewGetListByIdHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func NewToggleLikeHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func NewCreateNewHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
--- features/posts/delivery/http/handlers/handlers_test.go
+++ features/posts/delivery/http/handlers/handlers_test.go
@@ -1,1 +1,14 @@
 package handlers_test
+
+func TestGetListById(t *testing.T) {
+
+}
+func TestToggleLike(t *testing.T) {
+
+}
+func TestCreateNew(t *testing.T) {
+
+}
+func TestDelete(t *testing.T) {
+
+}
