95a01f13fd918d6c562daafb28971a94e61d18d4
Sam Komarov <sam@skomarov.com>
2022-06-15 08:50:35 +0300

    implement GetListById
    Former-commit-id: 18f3b785259e6a89ab868677a9710d1837464590

--- features/posts/delivery/http/handlers/handlers.go
+++ features/posts/delivery/http/handlers/handlers.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"core/client_errors"
 	helpers "core/http_helpers"
+	"encoding/json"
 	"net/http"
+	"posts/domain/entities"
 	"posts/domain/service"
 
 	"github.com/go-chi/chi/v5"
@@ -28,8 +30,24 @@ func NewDeleteHandler(deletePost service.PostDeleter) http.HandlerFunc {
 	})
 }
 
+type PostsResponse struct {
+	Posts []entities.Post
+}
+
 func NewGetListByIdHandler(getPosts service.PostsGetter) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		helpers.SetJsonHeader(w)
+		profileId := chi.URLParam(r, "profile_id")
+		if profileId == "" {
+			helpers.ThrowClientError(w, client_errors.IdNotProvided)
+			return
+		}
+		posts, err := getPosts(profileId)
+		if err != nil {
+			helpers.HandleServiceError(w, err)
+			return
+		}
+		json.NewEncoder(w).Encode(PostsResponse{posts})
 	})
 }
 
--- features/posts/delivery/http/handlers/handlers_test.go
+++ features/posts/delivery/http/handlers/handlers_test.go
@@ -9,13 +9,51 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"posts/delivery/http/handlers"
+	"posts/domain/entities"
 	"posts/domain/values"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
 )
 
+func createRequestWithProfileId(profileId core_values.UserId) *http.Request {
+	request := helpers.CreateRequest(nil)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("profile_id", profileId)
+	request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, ctx))
+	return request
+}
+
 func TestGetListById(t *testing.T) {
+	t.Run("happy case", func(t *testing.T) {
+		randomProfile := RandomString()
+		randomPosts := handlers.PostsResponse{
+			Posts: []entities.Post{RandomPost(), RandomPost(), RandomPost()},
+		}
+		getter := func(profileId core_values.UserId) ([]entities.Post, error) {
+			if profileId == randomProfile {
+				return randomPosts.Posts, nil
+			}
+			panic("unexpected args")
+		}
+		request := createRequestWithProfileId(randomProfile)
+		response := httptest.NewRecorder()
+		handlers.NewGetListByIdHandler(getter).ServeHTTP(response, request)
+		AssertJSONData(t, response, randomPosts)
+	})
+	t.Run("error case - profile id is not provided", func(t *testing.T) {
+		request := helpers.CreateRequest(nil)
+		response := httptest.NewRecorder()
+		handlers.NewGetListByIdHandler(nil).ServeHTTP(response, request)
+		AssertClientError(t, response, client_errors.IdNotProvided)
+	})
+	helpers.BaseTestServiceErrorHandling(t, func(err error, rr *httptest.ResponseRecorder) {
+		getter := func(core_values.UserId) ([]entities.Post, error) {
+			return []entities.Post{}, err
+		}
+		request := createRequestWithProfileId("42")
+		handlers.NewGetListByIdHandler(getter).ServeHTTP(rr, request)
+	})
 }
 func TestToggleLike(t *testing.T) {
 
