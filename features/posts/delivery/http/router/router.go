28ab4cdc0878494de18d06fb16d688f2dfac3f1d
Sam Komarov <sam@skomarov.com>
2022-06-18 09:15:31 +0300

    implement PostsRouter
    Former-commit-id: beefb00f2295c569c25a217ce7ae75c9d18ab2dc

--- features/posts/delivery/http/router/router.go
+++ features/posts/delivery/http/router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/k0marov/socnet/features/posts/delivery/http/handlers"
+	"github.com/k0marov/socnet/features/posts/domain/service"
+)
+
+func NewPostsRouter(create service.PostCreator, getPosts service.PostsGetter, deletePost service.PostDeleter, toggleLike service.PostLikeToggler) func(chi.Router) {
+	return func(r chi.Router) {
+		r.Post("/", handlers.NewCreateHandler(create))
+		r.Get("/?profile_id={profile_id}", handlers.NewGetListByIdHandler(getPosts))
+		r.Delete("/{id}", handlers.NewDeleteHandler(deletePost))
+		r.Post("/{id}/toggle-like", handlers.NewToggleLikeHandler(toggleLike))
+	}
+}
--- features/posts/posts.go
+++ features/posts/posts.go
@@ -1,1 +1,10 @@
 package posts
+
+import (
+	"database/sql"
+	"github.com/go-chi/chi/v5"
+)
+
+func NewPostsRouterImpl(db *sql.DB) func(chi.Router) {
+	panic("unimplemented")
+}
