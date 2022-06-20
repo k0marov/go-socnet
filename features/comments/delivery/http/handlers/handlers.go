74cf078ae56a5a1300c705ccccb2009a91820003
Sam Komarov <sam@skomarov.com>
2022-06-20 09:31:15 +0300

    implement ToggleLikeCommentHandler
    Former-commit-id: 876fa08a83528b3734dccd8186ca0238ce35a708

--- features/comments/delivery/http/handlers/handlers.go
+++ features/comments/delivery/http/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/k0marov/socnet/core/client_errors"
 	"github.com/k0marov/socnet/core/http_helpers"
 	"github.com/k0marov/socnet/features/comments/domain/entities"
@@ -33,8 +34,21 @@ func NewAddCommentHandler() http.HandlerFunc {
 	}
 }
 
-func NewToggleLikeCommentHandler() http.HandlerFunc {
+func NewToggleLikeCommentHandler(toggleLike service.CommentLikeToggler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		caller, ok := http_helpers.GetUserOrAddUnauthorized(w, r)
+		if !ok {
+			return
+		}
+		commentId := chi.URLParam(r, "id")
+		if commentId == "" {
+			http_helpers.ThrowClientError(w, client_errors.IdNotProvided)
+			return
+		}
+		err := toggleLike(commentId, caller.Id)
+		if err != nil {
+			http_helpers.HandleServiceError(w, err)
+			return
+		}
 	}
 }
--- features/comments/delivery/http/handlers/handlers_test.go
+++ features/comments/delivery/http/handlers/handlers_test.go
@@ -1,11 +1,16 @@
 package handlers_test
 
 import (
+	"context"
+	"github.com/go-chi/chi/v5"
+	auth "github.com/k0marov/golang-auth"
 	"github.com/k0marov/socnet/core/client_errors"
+	"github.com/k0marov/socnet/core/core_values"
 	helpers "github.com/k0marov/socnet/core/http_test_helpers"
 	. "github.com/k0marov/socnet/core/test_helpers"
 	"github.com/k0marov/socnet/features/comments/delivery/http/handlers"
 	"github.com/k0marov/socnet/features/comments/domain/entities"
+	"github.com/k0marov/socnet/features/comments/domain/values"
 	post_values "github.com/k0marov/socnet/features/posts/domain/values"
 	"net/http"
 	"net/http/httptest"
@@ -46,5 +51,37 @@ func TestNewAddCommentHandler(t *testing.T) {
 }
 
 func TestNewToggleLikeCommentHandler(t *testing.T) {
+	createRequestWithCommentId := func(id values.CommentId, caller auth.User) *http.Request {
+		request := helpers.CreateRequest(nil)
+		ctx := chi.NewRouteContext()
+		ctx.URLParams.Add("id", id)
+		request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, ctx))
+		return helpers.AddAuthDataToRequest(request, caller)
+	}
+	helpers.BaseTest401(t, handlers.NewToggleLikeCommentHandler(nil))
 
+	user := RandomAuthUser()
+	comment := RandomString()
+	t.Run("happy case", func(t *testing.T) {
+		likeToggler := func(commentId values.CommentId, caller core_values.UserId) error {
+			if commentId == comment && caller == user.Id {
+				return nil
+			}
+			panic("unexpected args")
+		}
+		response := httptest.NewRecorder()
+		handlers.NewToggleLikeCommentHandler(likeToggler).ServeHTTP(response, createRequestWithCommentId(comment, user))
+		AssertStatusCode(t, response, http.StatusOK)
+	})
+	t.Run("error case - id is not provided", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		handlers.NewToggleLikeCommentHandler(nil).ServeHTTP(response, helpers.AddAuthDataToRequest(helpers.CreateRequest(nil), user))
+		AssertClientError(t, response, client_errors.IdNotProvided)
+	})
+	helpers.BaseTestServiceErrorHandling(t, func(err error, response *httptest.ResponseRecorder) {
+		likeToggler := func(values.CommentId, core_values.UserId) error {
+			return err
+		}
+		handlers.NewToggleLikeCommentHandler(likeToggler).ServeHTTP(response, createRequestWithCommentId(comment, user))
+	})
 }
