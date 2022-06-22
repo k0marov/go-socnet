693da69ef3f8f66f8e068f3c3620bcf760c46033
Sam Komarov <sam@skomarov.com>
2022-06-22 07:12:42 +0300

    implement comments router
    Former-commit-id: b8c9974f0f4a95c8ec822495c5f3e3736320715e

--- features/comments/comments.go
+++ features/comments/comments.go
@@ -1,1 +1,10 @@
 package comments
+
+import (
+	"database/sql"
+	"github.com/go-chi/chi/v5"
+)
+
+func NewCommentsRouterImpl(db *sql.DB) func(chi.Router) {
+	panic("unimplemented")
+}
--- features/comments/delivery/http/router/router.go
+++ features/comments/delivery/http/router/router.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func NewCommentsRouter(getComments, createComment, toggleLike http.HandlerFunc) func(chi.Router) {
+	return func(r chi.Router) {
+		r.Get("/", getComments)
+		r.Post("/", createComment)
+		r.Post("/{id}/toggle-like", toggleLike)
+	}
+}
