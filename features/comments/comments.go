3b8eebbacd98da6c76ea5a7bc9c78f0e7601ae4d
Sam Komarov <sam@skomarov.com>
2022-06-22 07:17:59 +0300

    implement comments dependency injection
    Former-commit-id: a3eba1a72ef9b4ea492bb3dc61a2262ddd38f550

--- features/comments/comments.go
+++ features/comments/comments.go
@@ -3,8 +3,29 @@ package comments
 import (
 	"database/sql"
 	"github.com/go-chi/chi/v5"
+	"github.com/k0marov/socnet/features/comments/delivery/http/handlers"
+	"github.com/k0marov/socnet/features/comments/delivery/http/router"
+	"github.com/k0marov/socnet/features/comments/domain/service"
+	"github.com/k0marov/socnet/features/comments/domain/validators"
+	"github.com/k0marov/socnet/features/comments/store/sql_db"
+	profile_service "github.com/k0marov/socnet/features/profiles/domain/service"
+	"log"
 )
 
-func NewCommentsRouterImpl(db *sql.DB) func(chi.Router) {
-	panic("unimplemented")
+func NewCommentsRouterImpl(db *sql.DB, getProfile profile_service.ProfileGetter) func(chi.Router) {
+	// db
+	sqlDB, err := sql_db.NewSqlDB(db)
+	if err != nil {
+		log.Fatalf("error while opening sql db for comments: %w", err)
+	}
+	// service
+	validator := validators.NewCommentValidator()
+	getComments := service.NewPostCommentsGetter(sqlDB.GetComments, getProfile, sqlDB.IsLiked)
+	createComment := service.NewCommentCreator(validator, getProfile, sqlDB.Create)
+	toggleLike := service.NewCommentLikeToggler(sqlDB.GetAuthor, sqlDB.IsLiked, sqlDB.Like, sqlDB.Unlike)
+	// handlers
+	getCommentsHandler := handlers.NewGetCommentsHandler(getComments)
+	createCommentHandler := handlers.NewCreateCommentHandler(createComment)
+	toggleLikeHandler := handlers.NewToggleLikeCommentHandler(toggleLike)
+	return router.NewCommentsRouter(getCommentsHandler, createCommentHandler, toggleLikeHandler)
 }
