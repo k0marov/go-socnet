0658b2c02c0f646e0b9138e6d875980deda8d8fc
Sam Komarov <sam@skomarov.com>
2022-06-23 05:13:25 +0300

    implement NewLikeable
    Former-commit-id: 430cc1ac5d15581e33bbbe9ff9070497597befd8

--- core/likeable/likeable.go
+++ core/likeable/likeable.go
@@ -2,7 +2,10 @@ package likeable
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/k0marov/socnet/core/core_values"
+	"github.com/k0marov/socnet/core/likeable/service"
+	"github.com/k0marov/socnet/core/likeable/store/sql_db"
 	"github.com/k0marov/socnet/core/likeable/table_name"
 )
 
@@ -16,6 +19,19 @@ type likeable struct {
 	IsLiked       LikeChecker
 }
 
-func NewLikeable(db *sql.DB, targetTable table_name.TableName) likeable {
-	return likeable{}
+func NewLikeable(db *sql.DB, targetTableName string) (likeable, error) {
+	// store
+	store, err := sql_db.NewSqlDB(db, table_name.NewTableName(targetTableName))
+	if err != nil {
+		return likeable{}, fmt.Errorf("while opening the likeable sql db: %w", err)
+	}
+	// service
+	toggleLike := service.NewLikeToggler(store.IsLiked, store.Like, store.Unlike)
+	getLikesCount := service.NewLikesCountGetter(store.GetLikesCount)
+	isLiked := service.NewLikeChecker(store.IsLiked)
+	return likeable{
+		ToggleLike:    toggleLike,
+		GetLikesCount: getLikesCount,
+		IsLiked:       isLiked,
+	}, nil
 }
