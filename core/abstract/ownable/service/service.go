ffd7c6e206858fc3f73aa215c25d2490418ba3e9
Sam Komarov <sam@skomarov.com>
2022-07-06 15:49:04 +0300

    add returning NotFound client error from the ownable service

--- core/abstract/ownable/service/service.go
+++ core/abstract/ownable/service/service.go
@@ -1,11 +1,24 @@
 package service
 
-import "github.com/k0marov/go-socnet/core/general/core_values"
+import (
+	"github.com/k0marov/go-socnet/core/general/client_errors"
+	"github.com/k0marov/go-socnet/core/general/core_err"
+	"github.com/k0marov/go-socnet/core/general/core_values"
+)
 
 type StoreOwnerGetter func(targetId string) (core_values.UserId, error)
 
 type OwnerGetter func(targetId string) (core_values.UserId, error)
 
 func NewOwnerGetter(getOwner StoreOwnerGetter) OwnerGetter {
-	return OwnerGetter(getOwner)
+	return func(target string) (core_values.UserId, error) {
+		owner, err := getOwner(target)
+		if err == core_err.ErrNotFound {
+			return "", client_errors.NotFound
+		}
+		if err != nil {
+			return "", core_err.Rethrow("getting owner from store", err)
+		}
+		return owner, nil
+	}
 }
--- core/abstract/ownable/service/service_test.go
+++ core/abstract/ownable/service/service_test.go
@@ -1,1 +1,43 @@
 package service_test
+
+import (
+	"github.com/k0marov/go-socnet/core/abstract/ownable/service"
+	"github.com/k0marov/go-socnet/core/general/client_errors"
+	"github.com/k0marov/go-socnet/core/general/core_err"
+	. "github.com/k0marov/go-socnet/core/helpers/test_helpers"
+	"testing"
+)
+
+func TestOwnerGetter(t *testing.T) {
+	target := RandomString()
+	owner := RandomString()
+
+	getOwner := func(targetId string) (string, error) {
+		if targetId == target {
+			return owner, nil
+		}
+		panic("unexpected args")
+	}
+
+	t.Run("error case - getting owner returns ErrNotFound", func(t *testing.T) {
+		getOwner := func(string) (string, error) {
+			return "", core_err.ErrNotFound
+		}
+		_, err := service.NewOwnerGetter(getOwner)(target)
+		AssertError(t, err, client_errors.NotFound)
+	})
+	t.Run("error case - getting owner returns some other error", func(t *testing.T) {
+		getOwner := func(string) (string, error) {
+			return "", RandomError()
+		}
+		_, err := service.NewOwnerGetter(getOwner)(target)
+		AssertSomeError(t, err)
+	})
+
+	t.Run("happy case", func(t *testing.T) {
+		gotOwner, err := service.NewOwnerGetter(getOwner)(target)
+		AssertNoError(t, err)
+		Assert(t, gotOwner, owner, "returned owner")
+	})
+
+}
