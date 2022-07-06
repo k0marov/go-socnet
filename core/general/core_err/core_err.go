9a530ec5276d81e5b7a73e8fc8c298a9deedb87f
Sam Komarov <sam@skomarov.com>
2022-07-06 15:17:39 +0300

    return the same error from the Rethrow function if the error is a client error

--- core/general/core_err/core_err.go
+++ core/general/core_err/core_err.go
@@ -3,10 +3,15 @@ package core_err
 import (
 	"errors"
 	"fmt"
+	"github.com/k0marov/go-socnet/core/general/client_errors"
 )
 
 var ErrNotFound = errors.New("requested entity was not found")
 
 func Rethrow(description string, err error) error {
+	_, isClientError := err.(client_errors.ClientError)
+	if isClientError {
+		return err
+	}
 	return fmt.Errorf("%s: %w", description, err)
 }
